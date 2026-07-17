9611a5f17b2e9258cf398df5d169c9bc96ce63b5
Sylviabraide <Braidesylvia@gmail.com>
2026-07-17 12:31:47 +0100

    printing multiple values

--- modigotutorials/helloworld.py
+++ modigotutorials/helloworld.py
@@ -1,2 +1,2 @@
-# in the print function, we also about the end and seperate parameter function.
+# in the print function, we also learned about the end and seperate parameter function.
 print("name:", "Janet", "age:", 30, "favorite food:", "Alfredo pasta", "nationality:", "Nigerian", sep=" ", end=".\n")
